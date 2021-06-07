17639e3926e2e7b665100d3cb2746f0742f3888d
sebastian <sebastian bowen>
2021-06-06 23:37:01 -0600

    Modal tests complete

--- src/components/Modal/_tests_/index.test.js
+++ src/components/Modal/_tests_/index.test.js
@@ -1,5 +1,47 @@
-import {render, cleanup} from '@testing-library/react'
+import {render, cleanup, fireEvent} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Modal from '..'
 
+
+const currentPhoto = {
+    name: 'Park Bench',
+    category: 'landscape',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    index: 1     
+
+};
+const mockToggleModal= jest.fn()
+
+
 afterEach(cleanup);
+
+
+describe('Modal Component', () => {
+    it('renders', () => {
+        <Modal />
+    });
+
+    it('Matches snapshot dom node structure', () => {
+        const {asFragment} = render(
+            <Modal 
+            currentPhoto={currentPhoto}
+            toggleModal={mockToggleModal}
+            />
+        )
+        expect(asFragment()).toMatchSnapshot()
+    })
+
+});
+
+describe('Click Event', () => {
+    it('calls on close hanler', () => {
+        const {getByText} = render(<Modal 
+        
+        onClose={mockToggleModal}
+        currentPhoto={currentPhoto}
+        />);
+        fireEvent.click(getByText('Close this modal'));
+        expect(mockToggleModal).toHaveBeenCalledTimes(1);
+    })
+})
+
